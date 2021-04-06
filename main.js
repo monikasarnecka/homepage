fb962631717ab12d4a077a60c5a779fbfef2ba89
Monika Sarnecka <monika.sarnecka@interia.pl>
2021-04-06 22:14:08 +0200

    heading change

--- main.js
+++ main.js
@@ -1,6 +1,16 @@
-const firstName='Monika';
-const age=29;
+const firstName = "Monika";
+const age = 29;
 
 console.log(firstName);
 console.log(age);
 console.log(`Nazywam się ${firstName} i mam ${age} lat.`);
+
+
+const heading=document.querySelector('.header-first--js');
+
+console.log(heading.innerHTML);
+
+
+heading.innerHTML= `Nazywam się ${firstName} i mam ${age} lat.`;
+
+console.log(heading.innerHTML);
